0201c7fe40f36085ccfd4492b8640e0964885c5b
claudiodimauro <claudiodimauro@localhost>
2021-02-15 14:18:05 +0100

    Added parameters in application.properties Added attribute in Patter.java

--- src/main/java/com/claudiodimauro/Scrape4Engineering/api/models/Pattern.java
+++ src/main/java/com/claudiodimauro/Scrape4Engineering/api/models/Pattern.java
@@ -1,5 +1,17 @@
 package com.claudiodimauro.Scrape4Engineering.api.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Document(collection = "pattern")
 public class Pattern {
+    @Id
+    String id;
     //to define
 }
