17ec8a3ecb78d202be1c761f3f76fd034115aab4
User <User@MSI>
2021-02-15 18:04:25 +0100

    scrittura del modello pattern più aggiunta dei metodi di definizione di swagger per le classi entity e pattern

--- src/main/java/com/claudiodimauro/Scrape4Engineering/api/models/Entity.java
+++ src/main/java/com/claudiodimauro/Scrape4Engineering/api/models/Entity.java
@@ -1,5 +1,7 @@
 package com.claudiodimauro.Scrape4Engineering.api.models;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,14 +11,26 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Getter
 @Setter
 @Document(collection = "entity")
+@ApiModel(description = "")
 public class Entity {
+
+    @ApiModelProperty(notes = "")
     String entityId;
+    @ApiModelProperty(notes = "")
     String entityTitle;
+    @ApiModelProperty(notes = "")
     String basePath;
+    @ApiModelProperty(notes = "")
     String path;
+    @ApiModelProperty(notes = "")
     String url = basePath + path;
+    @ApiModelProperty(notes = "")
     String lastUpdate; //convert into Date type
+    @ApiModelProperty(notes = "")
     String lastScraping; //convert into Date type
+    @ApiModelProperty(notes = "")
     String content;
-    String [] attachmentId; //convert into ObjectId
+    @ApiModelProperty(notes = "")
+    String[] attachmentId; //convert into ObjectId
+    
 }
--- src/main/java/com/claudiodimauro/Scrape4Engineering/api/models/Pattern.java
+++ src/main/java/com/claudiodimauro/Scrape4Engineering/api/models/Pattern.java
@@ -1,5 +1,7 @@
 package com.claudiodimauro.Scrape4Engineering.api.models;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,8 +12,29 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Getter
 @Setter
 @Document(collection = "pattern")
+@ApiModel(description = "")
 public class Pattern {
+
+    @ApiModelProperty(notes = "")
     @Id
-    String id;
-    //to define
+    private String url;//id = website url
+    @ApiModelProperty(notes = "")
+    private String tagForBody;
+    @ApiModelProperty(notes = "")
+    private String tagForEntityId;
+    @ApiModelProperty(notes = "")
+    private String attrForEntityId;
+    @ApiModelProperty(notes = "")
+    private String tagForEntityTitle;
+    @ApiModelProperty(notes = "")
+    private Boolean selectorMethodForEntityTitle;//txt true or attr false
+    @ApiModelProperty(notes = "")
+    private String attrForEntityTitle;//(only to use when the method is attr)
+    @ApiModelProperty(notes = "")
+    private String entityPath;//(href)
+    @ApiModelProperty(notes = "")
+    private String lastEntityUpdate;
+    @ApiModelProperty(notes = "")
+    private String attrLastEntityUpdate;
+
 }
