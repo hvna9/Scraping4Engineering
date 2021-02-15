9777353f27b19ffba4a40a43d42b00984d69336e
claudiodimauro <claudiodimauro@localhost>
2021-02-15 13:32:40 +0100

    add new Class

---
+++ src/main/java/com/claudiodimauro/Scrape4Engineering/api/controllers/testController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.claudiodimauro.Scrape4Engineering.api.controllers;
+
+/**
+ *
+ * @author claudiodimauro
+ */
+public class testController {
+    
+}
